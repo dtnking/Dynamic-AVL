9fbdce60d4bb204fceaa3ff5acb050339334c370
Danny Aw <kokwah07@hotmail.com>
2018-01-15 18:17:09 +0800

    Aligned the code properly

--- src/avlRelativeAdd.c
+++ src/avlRelativeAdd.c
@@ -14,12 +14,6 @@ int avlRelativeAdd(Node **rootPtr, Node *nodeToAdd, int previousValue, int cummu
   }else{
     absoluteValue = previousValue + (*rootPtr)->cummulativeVal;
     if (absoluteValue < nodeToAdd->cummulativeVal){
-      // if((*rootPtr)->right != NULL){
-      //   previousValue += (*rootPtr)->right->cummulativeVal;
-      // }
-      // else{
-      //   previousValue = nodeToAdd->cummulativeVal - previousValue;
-      // }
       heightChanged = avlRelativeAdd(&(*rootPtr)->right,nodeToAdd,absoluteValue,nodeToAdd->cummulativeVal-absoluteValue);
       if(heightChanged==1){
           (*rootPtr)->balanceFactor += 1 ;
@@ -37,24 +31,25 @@ int avlRelativeAdd(Node **rootPtr, Node *nodeToAdd, int previousValue, int cummu
     else
       return 1;
   }
+
   else if(absoluteValue > nodeToAdd->cummulativeVal){
     heightChanged = avlRelativeAdd(&(*rootPtr)->left,nodeToAdd,previousValue,nodeToAdd->cummulativeVal- previousValue);
-      if((*rootPtr)->left != NULL){
-        if((*rootPtr)->left->cummulativeVal == addCummulativeVal || (*rootPtr)->left->cummulativeVal + previousValue == addCummulativeVal)
-        (*rootPtr)->relativeVal -= cummulativeValue;
-      }
-      if((*rootPtr)->left->right !=NULL){
-        int addRelativeVal = findAddRelativeVal(&(*rootPtr)->left,nodeToAdd,addCummulativeVal,previousValue);
-        (*rootPtr)->relativeVal -= addRelativeVal;
-      }
+    if((*rootPtr)->left != NULL){
+      if((*rootPtr)->left->cummulativeVal == addCummulativeVal || (*rootPtr)->left->cummulativeVal + previousValue == addCummulativeVal)
+      (*rootPtr)->relativeVal -= cummulativeValue;
+    }
+    if((*rootPtr)->left->right !=NULL){
+      int addRelativeVal = findAddRelativeVal(&(*rootPtr)->left,nodeToAdd,addCummulativeVal,previousValue);
+      (*rootPtr)->relativeVal -= addRelativeVal;
+    }
 
-      if(heightChanged==1){
-        (*rootPtr)->balanceFactor -= 1;
-        if((*rootPtr)->balanceFactor==0)
-          return 0;
-      }
-      else
+    if(heightChanged==1){
+      (*rootPtr)->balanceFactor -= 1;
+      if((*rootPtr)->balanceFactor==0)
         return 0;
+    }
+    else
+      return 0;
 
     if((*rootPtr)->balanceFactor !=-2)
       return 1;
@@ -63,11 +58,30 @@ int avlRelativeAdd(Node **rootPtr, Node *nodeToAdd, int previousValue, int cummu
       return 0;
     else
       return 1;
-    }
-
-
-
-      //Throw(createException("Node to add is already exist", NODE_ADD_EXIST));
   }
-
+  // else
+  //   Throw(createException("Node to add is already exist", NODE_ADD_EXIST));
 }
+}
+
+
+
+
+//
+// int myfunc(int count, ...)
+// {
+//    va_list list;
+//    Node *root = NULL;
+//    int j = 0;
+//
+//    va_start(list, count);
+//    for(j=0; j<count; j++)
+//    {
+//      avlRelativeAdd(&root,&list,0,list->cummulativeVal);
+//      printf("%d", va_arg(list.relativeVal, int));
+//    }
+//
+//    va_end(list);
+//
+//    return count;
+// }
