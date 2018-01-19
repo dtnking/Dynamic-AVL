2895a730ac37f2dcb71b32194b2255fa70eeba92
Danny Aw <kokwah07@hotmail.com>
2018-01-19 23:21:41 +0800

    Added test for removing smallest node when the smallest is the root

--- test/test_NodeHelper.c
+++ test/test_NodeHelper.c
@@ -12,7 +12,7 @@ void setUp(void){}
 
 void tearDown(void){}
 
-void test_findSmallestNode(void){
+void test_findSmallestRelativeVal_given_Node_6_3_9_8_10_expected_return_3(void){
   Node *root = &node6;
   int result;
   initNode(&node6,&node3,&node9,1,3,6);
@@ -23,4 +23,18 @@ void test_findSmallestNode(void){
 
   result = findSmallestRelativeVal(&root);
   TEST_ASSERT_EQUAL_PTR(3,result);
+}
+
+/*            (6/6)6
+ *                 \
+ *                (4/4)10
+ */
+void test_findSmallestRelativeVal_given_Node_6_10_expected_return_6(void){
+  Node *root = &node6;
+  int result;
+  initNode(&node6,NULL,&node10,1,6,6);
+  initNode(&node10,NULL,NULL,0,4,4);
+
+  result = findSmallestRelativeVal(&root);
+  TEST_ASSERT_EQUAL_PTR(6,result);
 }
