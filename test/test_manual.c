3f884aa5c89239974cade27c74819bcf120f1aa3
Danny Aw <kokwah07@hotmail.com>
2018-01-24 17:10:09 +0800

    Added another random scenario for functional test

--- test/test_manual.c
+++ test/test_manual.c
@@ -11,7 +11,7 @@
 
 Node node1,node2,node3,node4,node5,node6,node7,node8,node9,node10;
 Node node11,node12,node13,node14,node15,node16,node17,node18,node19,node20;
-Node node28,node30,node40,node80;
+Node node28,node30,node40,node80,node31,node22,node34,node37,node45,node25;
 
 void setUp(void){}
 
@@ -38,6 +38,46 @@ void test_manual_add_4_2_15_10_30_80_28_40_3(void){
   avlRelativeAdd(&root,&node28);
   avlRelativeAdd(&root,&node40);
   avlRelativeAdd(&root,&node3);
+
+  printRelValFromSmallest(&root);
+}
+
+
+void test_manual_given_random_node(void){
+  Node *root = NULL;
+  createNode(&node3,3);
+  createNode(&node1,1);
+  createNode(&node20,20);
+  createNode(&node2,2);
+  createNode(&node6,6);
+  createNode(&node31,31);
+  createNode(&node22,22);
+  createNode(&node34,34);
+  createNode(&node14,14);
+  createNode(&node37,37);
+  createNode(&node9,9);
+  createNode(&node40,40);
+  createNode(&node45,45);
+  createNode(&node25,25);
+  createNode(&node8,8);
+  createNode(&node17,17);
+
+  avlRelativeAdd(&root,&node3);
+  avlRelativeAdd(&root,&node1);
+  avlRelativeAdd(&root,&node20);
+  avlRelativeAdd(&root,&node2);
+  avlRelativeAdd(&root,&node6);
+  avlRelativeAdd(&root,&node31);
+  avlRelativeAdd(&root,&node22);
+  avlRelativeAdd(&root,&node34);
+  avlRelativeAdd(&root,&node14);
+  avlRelativeAdd(&root,&node37);
+  avlRelativeAdd(&root,&node9);
+  avlRelativeAdd(&root,&node40);
+  avlRelativeAdd(&root,&node45);
+  avlRelativeAdd(&root,&node25);
+  avlRelativeAdd(&root,&node8);
+  avlRelativeAdd(&root,&node17);
 
   printRelValFromSmallest(&root);
 }
